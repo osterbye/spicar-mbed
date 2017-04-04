b13406b4d102d308959313adeaabdf15c305703e
Nikolaj Due Oesterbye <nikolaj@due-oesterbye.dk>
2017-04-04 13:21:09 +0200

    Timer based loop delay

--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 
 DigitalOut led1(LED1);
 Serial pc(USBTX, USBRX);
+Timer waitTimer;
 
 int main() {
+    const int loopTime = 1000;
     bool abort = false;
+
+    waitTimer.start();
 
     while(!abort) {
         led1 = !led1;
         pc.printf("This is a print\r\n");
-        Thread::wait(1000);
+        
+        Thread::wait((loopTime - waitTimer.read_ms()));
+        waitTimer.reset();
     }    
 }
 
